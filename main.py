0ef2e53f1ea7b932c70221ca77c571560a745295
Esteban Ordano <eordano@gmail.com>
2014-05-17 17:09:49 -0300

    Add skeleton of UI

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+import pygame.locals as pg
+
+import car
+import lane
+import trafficlight
+import control
+
+
+pygame.init()
+pygame.font.init()
+
+screen = pygame.display.set_mode((1200, 400))
+
+def draw_data():
+    pass
+
+def draw_lanes():
+    pass
+
+def draw_cars():
+    pass
+
+def advance_cars():
+    pass
+
+def print_screen():
+    pass
+
+while True:
+    draw_data()
+    draw_lanes()
+    draw_cars()
+    advance_cars()
+    print_screen()
